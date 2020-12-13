555a231bf7afdf7b831524ec1a45c61f4784dbca
Adam Lyon <lyon@fnal.gov>
2020-12-12 19:32:42 -0600

    Additional packages for Mu2e

--- build_all.sh
+++ build_all.sh
@@ -2,13 +2,12 @@
 
 # Build all of the images in the correct order
 
-cd devenv_sl6
-docker build -t lyonfnal/devenv:sl6 .
+#cd devenv_sl6
+#docker build -t lyonfnal/devenv:sl6 .
 
-cd ../devenv_sl7
+cd devenv_sl7
 docker build -t lyonfnal/devenv:sl7 .
 
 cd ../devenv-cvmfs
-docker build -t lyonfnal/devenv_cvmfs:sl6 -f Dockerfile-sl6 .
+#docker build -t lyonfnal/devenv_cvmfs:sl6 -f Dockerfile-sl6 .
 docker build -t lyonfnal/devenv_cvmfs:sl7 -f Dockerfile-sl7 .
- 
--- push_all.sh
+++ push_all.sh
@@ -3,8 +3,8 @@
 # Push all images
 
 # Note that we must push all of them (not just the bottom of the dependency tree) in order to get the tags right.
-docker push lyonfnal/devenv:sl6
-docker push lyonfnal/devenv_cvmfs:sl6
+#docker push lyonfnal/devenv:sl6
+#docker push lyonfnal/devenv_cvmfs:sl6
 
 docker push lyonfnal/devenv:sl7
 docker push lyonfnal/devenv_cvmfs:sl7
